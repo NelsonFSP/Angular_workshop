f26da7e7d2ab449c2f04508c5b3a5b3bc6909ebb
NelsonFSP <nelson.santosdepaula@yahoo.com.br>
2020-07-15 23:24:19 -0300

    second commit with new components and service

---
+++ src/app/shared/model/live.model.ts
@@ -0,0 +1,9 @@
+export class Live{
+    id: String;
+    liveName: string;
+    channelName: string;
+    liveDate: string;
+    liveTime: string;
+    liveLink:string;
+    registrationDate:string;
+}
---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,12 @@
+export class ResponsePageable{
+    content: any[];
+    first: boolean;
+    last: boolean;
+    number: number;
+    numberofElements: number;
+    pageable: any[];
+    size: number;
+    sort:number;
+    totalElements:number;
+    totalPages:number;
+}
